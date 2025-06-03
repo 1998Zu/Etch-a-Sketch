f2417880999ecf13d5257891e6a2186dd57c802c
Zuzizwe <207649087+1998Zu@users.noreply.github.com>
2025-06-03 03:45:33 +0200

    Complete core functionality of Etch-A-Sketch

--- JavaScript.js
+++ JavaScript.js
@@ -2,27 +2,40 @@ const container = document.querySelector("#containerGrid");
 
 const div = document.createElement("div");
 
-const numberOfCopies = 256;
-
-for (let i = 0; i < numberOfCopies; i++){
-    const clonedDiv = div.cloneNode(true);
-    clonedDiv.classList.add("gridSquare");
-    container.appendChild(clonedDiv)
-
-    clonedDiv.addEventListener("mouseover", function(){
-    clonedDiv.style.backgroundColor = "black";
-    
-    });
-    
-}
-
-const button = document.querySelector("#sizeBtn")
+const button = document.querySelector("#sizeBtn");
 
 button.onclick = function changeSize(){
-  let userInput =  prompt("Pick the number of squares per side");
-   
-}
+    let userInput =  parseInt(prompt("Pick the number of squares per side between 1 and 100"));
+    
+    let squareSize = (500 / userInput) -2 ;
+    
+    container.innerHTML ="";
+    
+    
+    const numberOfCopies = userInput * userInput;
+
+    if( userInput > 100 || userInput < 0 || isNaN(userInput)){
+        alert("ERROR!! Pick a number between 1 and 100");
+        return;
+    }
 
 
+    for (let i = 0; i < numberOfCopies; i++){
 
+     const clonedDiv = div.cloneNode(true);
+     clonedDiv.classList.add("gridSquare");
+     container.appendChild(clonedDiv);
 
+     clonedDiv.style.width = `${squareSize}px`;
+     clonedDiv.style.height = `${squareSize}px`;
+
+     clonedDiv.addEventListener("mouseover", function(){
+     clonedDiv.style.backgroundColor = "black";
+
+     });
+
+     }
+
+    
+
+};
